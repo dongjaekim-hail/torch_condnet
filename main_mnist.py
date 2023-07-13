1a25c473ae97f13c2977650dade271e1e97b1ff4
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-07-13 23:12:25 +0900

    upload

--- main_mnist.py
+++ main_mnist.py
@@ -7,16 +7,14 @@ from torchvision import transforms, datasets # 데이터를 다루기 위한 Tor
 
 import torch.nn as nn
 import torch.nn.functional as F
+import wandb
 
-nlayers = 2
-nfilters = 96
-lambda_s = 100
-lambda_v = 10
-tau = 0.5
-lr = 0.003
-max_epochs = 1000
-condnet_min_prob = 0.5
-condnet_max_prob = 1
+from datetime import datetime
+
+# get time in string to save as file name
+now = datetime.now()
+dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+
 
 class policyNet(nn.Module):
     def __init__(self, n_layer=4, hidden_s = [1024, 1024, np.nan]):
@@ -26,17 +24,42 @@ class policyNet(nn.Module):
             self.layers.append(nn.Linear(32*32*3, 32*32*3))
 
 
-class model_condnet(nn.Module):
+
+class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        input_dim = 32*32*3
+        self.fc1 = nn.Linear(32*32*3, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        # flatten
+        x = x.view(-1, 32*32*3)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # softmax
+        x = F.softmax(x, dim=1)
+        return x
+
+
+class model_condnet(nn.Module):
+    def __init__(self,args):
+        super().__init__()
+        self.input_dim = 28*28
         mlp_hidden = 1024
         output_dim = 10
+
+        nlayers = args.nlayers
+        self.condnet_min_prob = args.condnet_min_prob
+        self.condnet_max_prob = args.condnet_max_prob
+
 
         self.mlp_nlayer = 0
 
         self.mlp = nn.ModuleList()
-        self.mlp.append(nn.Linear(input_dim, mlp_hidden))
+        self.mlp.append(nn.Linear(self.input_dim, mlp_hidden))
         for i in range(nlayers):
             self.mlp.append(nn.Linear(mlp_hidden, mlp_hidden))
         self.mlp.append(nn.Linear(mlp_hidden, output_dim))
@@ -45,7 +68,7 @@ class model_condnet(nn.Module):
         # n_each_policylayer = 1 # if you have only 1 layer perceptron for policy net
         self.policy_net = nn.ModuleList()
         temp = nn.ModuleList()
-        temp.append(nn.Linear(input_dim, mlp_hidden))
+        temp.append(nn.Linear(self.input_dim, mlp_hidden))
         temp.append(nn.Linear(mlp_hidden, mlp_hidden))
         self.policy_net.append(temp)
 
@@ -60,7 +83,7 @@ class model_condnet(nn.Module):
         sample_probs = []
         layer_masks = []
 
-        x = x.view(-1, 32*32*3)
+        x = x.view(-1, self.input_dim)
 
         # for each layer
         h = x
@@ -76,11 +99,11 @@ class model_condnet(nn.Module):
                 p_i = self.policy_net[i][j](p_i)
                 p_i = F.sigmoid(p_i)
 
-            p_i = p_i * (condnet_max_prob - condnet_min_prob) + condnet_min_prob
+            p_i = p_i * (self.condnet_max_prob - self.condnet_min_prob) + self.condnet_min_prob
             u_i = torch.bernoulli(p_i)
 
             # debug[TODO]
-            u_i = torch.ones(u_i.shape[0], u_i.shape[1])
+            # u_i = torch.ones(u_i.shape[0], u_i.shape[1])
 
             if u_i.sum() == 0:
                 idx = np.random.uniform(0, u_i.shape[0], size = (1)).astype(np.int16)
@@ -104,22 +127,33 @@ class model_condnet(nn.Module):
         h = F.softmax(h, dim=1)
         return h, policies, sample_probs, layer_masks
 
-def main():
-    # main for the condnet
+def main(args):
+    # get args
+    lambda_s = args.lambda_s
+    lambda_v = args.lambda_v
+    lambda_l2 = args.lambda_l2
+    lambda_pg = args.lambda_pg
+    tau = args.tau
+    learning_rate = args.lr
+    max_epochs = args.max_epochs
+    BATCH_SIZE = args.BATCH_SIZE
 
-    BATCH_SIZE = 1000
-    train_dataset = datasets.CIFAR10(
-        root="../data/cifar10",
+
+    # datasets load mnist data
+    train_dataset = datasets.MNIST(
+        root="../data/mnist",
         train=True,
         download=True,
         transform=transforms.ToTensor()
     )
-    test_dataset = datasets.CIFAR10(
-        root="../data/cifar10",
+
+    test_dataset = datasets.MNIST(
+        root="../data/mnist",
         train=False,
         download=True,
         transform=transforms.ToTensor()
     )
+
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
         batch_size=BATCH_SIZE,
@@ -132,18 +166,9 @@ def main():
     )
 
     # create model
-    model = model_condnet()
+    model = model_condnet(args)
     # model = model_condnet2()
-    learning_rate = 0.1
 
-
-    # lambda_s, lambda_v, lambda_l2
-    lambda_s = 1e-1 #100
-    lambda_v = 1e-3 #10
-    tau = 0.8
-    # lambda_l2 = 5e-4
-    lambda_l2 = 0
-    lambda_pg = 1e-5
 
     C = nn.CrossEntropyLoss()
     mlp_optimizer = optim.SGD(model.parameters(), lr=learning_rate,
@@ -181,13 +206,6 @@ def main():
 
             c = C(outputs, labels)
 
-            # L =  c + lambda_s * \
-            #      torch.add( (((torch.stack(policies)).mean(axis=1)-tau)**2).mean(), \
-            #        (((torch.stack(policies)).mean(axis=2)-tau)**2).mean()) + \
-            #          lambda_v * (-1) * torch.add(((torch.stack(sample_probs)).var(axis=1)).mean(),\
-            #                         ((torch.stack(sample_probs)).var(axis=2)).mean())
-            #     # batch L_b and
-
             # Compute the regularization loss L
 
             L = c + lambda_s * (torch.pow(torch.stack(policies).mean(axis=1) - tau, 2).mean() +
@@ -201,10 +219,7 @@ def main():
             logp = torch.log(torch.cat(sample_probs)).sum(axis=1).mean()
             PG = lambda_pg * c * (-logp) + L
 
-            # logp = torch.log(torch.concatenate(sample_probs)).sum(axis=1).mean()
-            # PG = c * (-logp) + L
             PG.backward() # it needs to be checked [TODO]
-            # c.backward()
             mlp_optimizer.step()
             # policy_optimizer.step()
 
@@ -222,127 +237,45 @@ def main():
         # print epoch and epochs costs and accs
         print('Epoch: {}, Cost: {}, Accuracy: {}'.format(epoch, costs / bn, accs / bn))
 
-        # calculate accuracy on test set
-        acc = 0
-        bn = 0
-        for i, data in enumerate(test_loader, 0):
-            bn += 1
-            # get batch
-            inputs, labels = data
+        model.eval()
+        with torch.no_grad():
+            # calculate accuracy on test set
+            acc = 0
+            bn = 0
+            for i, data in enumerate(test_loader, 0):
+                bn += 1
+                # get batch
+                inputs, labels = data
 
-            # make one hot vector
-            y_batch_one_hot = torch.zeros(labels.shape[0], 10)
-            y_batch_one_hot[torch.arange(labels.shape[0]), labels.reshape(-1,).tolist()] = 1
+                # make one hot vector
+                y_batch_one_hot = torch.zeros(labels.shape[0], 10)
+                y_batch_one_hot[torch.arange(labels.shape[0]), labels.reshape(-1,).tolist()] = 1
 
-            # get output
-            output,_,_,_ = model(torch.tensor(inputs))
+                # get output
+                output,_,_,_ = model(torch.tensor(inputs))
 
-            # calculate accuracy
-            pred = torch.argmax(output, dim=1)
-            acc += torch.sum(pred == torch.tensor(labels.reshape(-1))).item() / labels.shape[0]
-        #print accuracyt
-        print('Test Accuracy: {}'.format(acc / bn))
-
-
-
-def main_():
-
-    BATCH_SIZE = 1024
-    train_dataset = datasets.CIFAR10(
-        root="../data/cifar10",
-        train=True,
-        download=True,
-        transform=transforms.ToTensor()
-    )
-    test_dataset = datasets.CIFAR10(
-        root="../data/cifar10",
-        train=False,
-        download=True,
-        transform=transforms.ToTensor()
-    )
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True
-    )
-    test_loader = torch.utils.data.DataLoader(
-        dataset=test_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=False
-    )
-
-    # create model
-    model = Net()
-    learning_rate = 0.01
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate,
-                          momentum=0.9, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
-    # run for 50 epochs
-    for epoch in range(max_epochs):
-
-        model.train()
-        costs = 0
-        accs = 0
-
-        bn = 0
-        # run for each batch
-        for i, data in enumerate(train_loader, 0):
-            bn += 1
-            # get batch
-            inputs, labels = data
-
-            # 변화도(Gradient) 매개변수를 0으로 만들고
-            optimizer.zero_grad()
-
-            # 순전파 + 역전파 + 최적화를 한 후
-            outputs = model(inputs)
-
-            # make labels one hot vector
-            y_one_hot = torch.zeros(labels.shape[0], 10)
-            y_one_hot[torch.arange(labels.shape[0]), labels.reshape(-1)] = 1
-
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # calculate accuracy
-            pred = torch.argmax(outputs, dim=1)
-            acc = torch.sum(pred == torch.tensor(labels.reshape(-1))).item() / labels.shape[0]
-
-            # addup loss and acc
-            costs += loss.item()
-            accs += acc
-
-        scheduler.step()
-
-        # print epoch and epochs costs and accs
-        print('Epoch: {}, Cost: {}, Accuracy: {}'.format(epoch, costs / bn, accs / bn))
-
-        # calculate accuracy on test set
-        acc = 0
-        bn = 0
-        for i, data in enumerate(test_loader, 0):
-            bn += 1
-            # get batch
-            inputs, labels = data
-
-            # make one hot vector
-            y_batch_one_hot = torch.zeros(labels.shape[0], 10)
-            y_batch_one_hot[torch.arange(labels.shape[0]), labels.reshape(-1,).tolist()] = 1
-
-            # get output
-            output = model(torch.tensor(inputs))
-
-            # calculate accuracy
-            pred = torch.argmax(output, dim=1)
-            acc += torch.sum(pred == torch.tensor(labels.reshape(-1))).item() / labels.shape[0]
-        #print accuracyt
-        print('Test Accuracy: {}'.format(acc / bn))
-
-
+                # calculate accuracy
+                pred = torch.argmax(output, dim=1)
+                acc += torch.sum(pred == torch.tensor(labels.reshape(-1))).item() / labels.shape[0]
+            #print accuracyt
+            print('Test Accuracy: {}'.format(acc / bn))
 
 if __name__=='__main__':
-    main()
+    # make arguments and defaults for the parameters
+    import argparse
+    args = argparse.ArgumentParser()
+    args.add_argument('--nlayers', type=int, default=3)
+    args.add_argument('--lambda_s', type=float, default=100)
+    args.add_argument('--lambda_v', type=float, default=10)
+    args.add_argument('--lambda_l2', type=float, default=5e-4)
+    args.add_argument('--lambda_pg', type=float, default=1e-5)
+    args.add_argument('--tau', type=float, default=0.5)
+    args.add_argument('--lr', type=float, default=0.01)
+    args.add_argument('--max_epochs', type=int, default=1000)
+    args.add_argument('--condnet_min_prob', type=float, default=0.2)
+    args.add_argument('--condnet_max_prob', type=float, default=0.8)
+    args.add_argument('--learning_rate', type=float, default=0.1)
+    args.add_argument('--BATCH_SIZE', type=int, default=512)
+
+    wandb.init(project="conditional_networks")
+    main(args=args.parse_args())
